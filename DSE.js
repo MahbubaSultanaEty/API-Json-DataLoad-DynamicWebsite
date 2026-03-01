47d0fdb2f51615a5d7602273def0246007a239bf
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-01 22:35:34 +0600

    first commit

---
+++ DSE.js
@@ -0,0 +1,10 @@
+// 1: IP Address .
+// Every device or server on the Internet has a unique number called an IP address.Computers use this number to find each other.
+
+// DNS .
+/*DNS is like the Internet’s phonebook.  A domain name is a human-friendly name for a website. Its job: translate domain names into IP addresses.
+When you type google.com in your browser:
+The browser asks a DNS server: “What is the IP for google.com?”
+The DNS server replies with the IP.
+Your browser connects to that IP to load the website.
+*/
