b041784da7489909030e2179a691a9d4baa5f24f
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-03-02 21:44:53 +0600

    get and post

---
+++ theoritical/get-post.js
@@ -0,0 +1,16 @@
+/*1. GET
+
+Purpose: Data read / fetch korte use hoy.
+Body: Usually no body.
+Idempotent:  (Same request multiple times same result).
+Example: Fetching user info
+*/
+
+
+/*2. POST
+
+Purpose: New data create korte use hoy.
+Body: Required (data send korte).
+Idempotent: ❌ (Same request multiple times → multiple entries).
+Example: Create new user
+*/
